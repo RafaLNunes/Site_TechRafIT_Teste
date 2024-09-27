6903ebd1cf66d794fb68ee44dfe8ef3fa9b0a632
RafaLNunes <rafaf.code@gmail.com>
2024-09-27 11:15:55 -0300

    feito

--- App_Web_Test/PHP/Cad.php
+++ App_Web_Test/PHP/Cad.php
@@ -9,25 +9,34 @@ $Remetente = $_GET["RemetenteQ"];
 $txtarea = $_GET["OBS"];
 $confirm = $_GET["BntRd"];
 $Gen = $_GET["slc_gen"];
-$ft_cam = $_FILES["FT"]["tmp_name"];
 
 
+if (isset($_FILES["ft"]) && $_FILES["ft"]["error"] == 0) {
+    $ft_name = $_FILES["ft"]["name"];
+    $ft_cam = $_FILES["ft"]["tmp_name"];
 
-echo $ft_cam;
+    echo $ft_cam;
 
-if($Nome == ""){
-    echo "Campo Invalido";
-}else{
-    echo "<br>Hello Wolrd $Remetente";
-    echo "<br>Menssagem vai para: $Nome";
-    echo "<br>Assunto: $Assunto";
-    echo "<br>Menssagem: $txtarea";
-    echo "<br>Genero: $Gen";
-    echo "<br>Foto: ";
-    move_uploaded_file(from: $ft_cam,to: "/upload/");
-    echo "<img scr=$ft_cam>";
-    
+    if ($Nome == "") {
+        echo "Campo Invalido";
+    } else {
+        echo "<br>Hello Wolrd $Remetente";
+        echo "<br>Menssagem vai para: $Nome";
+        echo "<br>Assunto: $Assunto";
+        echo "<br>Menssagem: $txtarea";
+        echo "<br>Genero: $Gen";
+        echo "<br>Foto: ";
+        $ft_complet = move_uploaded_file(from: $ft_cam, to: "/upload/" . $ft_name);
+        echo "<img scr=>";
+
+    }
+} else {
+    echo "Erro ao enviar o arquivo";
 }
 
 
-?>  
+
+
+
+
+?>
